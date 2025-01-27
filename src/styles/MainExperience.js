8060a5359d036b22675eb2feacf590ccced9f368
vas-alina <vasalina93@gmail.com>
2025-01-27 23:08:45 +0300

    Updates

--- src/styles/MainExperience.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ExperienceContainer = styled.div`
-    border-radius: 8px;
-    padding: 20px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
